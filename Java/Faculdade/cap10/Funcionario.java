74a078368e9cdb3003b8867b09061b661b279e2e
Fernando <fernando.rita@outlook.com>
2025-07-25 11:46:51 -0300

    Organizando em pastas

---
+++ Java/Faculdade/cap10/Funcionario.java
@@ -0,0 +1,14 @@
+package cap10;
+
+public class Funcionario extends PessoaFisica {
+    private String cartao;
+
+    public String getCartao() {
+        return cartao;
+    }
+
+    public void setCartao(String cartao) {
+        this.cartao = cartao;
+    }
+
+}
---
+++ Java/Faculdade/cap10/PessoaJuridica.java
@@ -0,0 +1,14 @@
+package cap10;
+
+public class PessoaJuridica extends Contribuinte {
+    private String cnpj;
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+}
